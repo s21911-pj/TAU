6f2e9b180c2bfc2d4c4a63532d0ddc071db288a3
Piotr <s21911@pjwstk.edu.pl>
2022-10-23 10:36:11 +0200

    added 2 tests and new method to calculator

---
+++ Lab2/Calculator/src/main/java/Calculator.java
@@ -0,0 +1,65 @@
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+
+    public long multiply(long a, long b) {
+        return a * b;
+    }
+
+
+    public double divide(double a, double b) {
+        double result;
+        if (b == 0) {
+            throw new IllegalArgumentException("Divisor cannot divide by zero");
+        } else {
+            result = a/b;
+        }
+        return result;
+    }
+
+    public long factorialUsingForLoop(long n) {
+        long fact = 1;
+        if (n > 20) {
+            throw new IllegalArgumentException("To big number for long type");
+        }else
+            for (long i = 2; i <= n; i++) {
+                fact = fact * i;
+            }
+
+            return fact;
+        }
+
+
+    public int remainder(int a, int b) {return a % b;}
+
+    public float exponentiation(float a, float b)
+    {
+        float base=a;
+        float power=b;
+        int result=1;
+        if(power!=0){
+            for(int i=1;i<=power;i++)
+            {
+                result*=base;
+            }
+        }else if(power == 0) {
+            throw new IllegalArgumentException("Wrong number!!!!");
+        }
+        return result;
+        }
+    }
+
+
+
+
+
---
+++ Lab2/Calculator/src/test/java/TestCalculator.java
@@ -0,0 +1,108 @@
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Before;
+
+public class TestCalculator {
+    private Calculator objCalcUnderTest;
+
+    @Before
+    public void setUp() {
+
+        objCalcUnderTest = new Calculator();
+
+    }
+
+    @Test
+    public void testAdd() {
+        int a = 15; int b = -20;
+        int expectedResult = -5;
+
+        long result = objCalcUnderTest.add(a, b);
+        Assert.assertEquals(expectedResult, result);
+
+    }
+
+    @Test
+    public void testFactorial() {
+        long n = 19;
+        long expectedResult = 121645100408832000L;
+        long result = objCalcUnderTest.factorialUsingForLoop(n);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tooBigNumberForFactorialMethod() {
+        long n = 21;
+        objCalcUnderTest.factorialUsingForLoop(n);
+
+    }
+
+    @Test
+    public void testSubtract() {
+        int a = 25; int b = 20;
+        int expectedResult = 5;
+        long result = objCalcUnderTest.subtract(a, b);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+
+    @Test
+    public void testMultiply() {
+        int a = 10; int b = 25;
+        long expectedResult = 250;
+        long result = objCalcUnderTest.multiply(a, b);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testDivide() {
+        int a = 56; int b = 10;
+        double expectedResult = 5.6;
+        double result = objCalcUnderTest.divide(a, b);
+        Assert.assertEquals(expectedResult, result,0.00005);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        int a = 15; int b = 0;
+        objCalcUnderTest.divide(a, b);
+    }
+    @Test
+    public void  shouldGivethree(){
+    objCalcUnderTest.divide(8,5);
+    Assert.assertEquals(3,3);
+    }
+    @Test
+    public void testRemainder() {
+        int a = 13;
+        int b = 4;
+        int expectedResult = 1;
+        int result = objCalcUnderTest.remainder(a, b);
+        Assert.assertEquals(expectedResult, result);
+    }
+    @Test
+    public void testExponentiation(){
+        float a = 5;
+        float b = 2;
+        float expectedResult = 25;
+        float result = objCalcUnderTest.exponentiation(a,b);
+        Assert.assertEquals(expectedResult,result,0.00000001);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testExponnentiationWithZero(){
+        float a = 5;
+        float b = 0;
+        objCalcUnderTest.exponentiation(a,b);
+    }
+
+
+    @After
+    public void after() {
+
+        System.out.println("Jesli mnie widzisz znaczy ze to dzialanie jest poprawne :) ");
+
+    }
+}
