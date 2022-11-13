4e60142ae5d4424422ba2442cc80a5ce024b9c2e
Piotr <s21911@pjwstk.edu.pl>
2022-11-13 13:34:37 +0100

    some progress , added writing on login  page

--- Lab3/main.py
+++ Lab3/main.py
@@ -4,6 +4,8 @@ from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 import logging
 
+from selenium.webdriver.remote.webelement import WebElement
+
 logger = logging.getLogger('simple_example')
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
@@ -18,7 +20,9 @@ driver = webdriver.Chrome(executable_path='D:\TAU\Lab3\chromedriver.exe')
 logger.info('Going on site armyworld.pl')
 driver.get('https://www.armyworld.pl/')
 checktools = driver.find_element(By.XPATH, '//*[@id="menu_navbar"]/ul/li[3]/a').click()
+#click narzedzia on site
 checkToolByTakeItToBasket = driver.find_element(By.XPATH, '//*[@id="search"]/div[1]/div[2]/form/button').click()
+#add to basket
 checkBasket = driver.find_element(By.XPATH, '//*[@id="menu_basket"]/a').click()
 #checking is the item is in basket
 checkIteminBasket = driver.find_element(By.XPATH, '//*[@id="Basket"]/form/div[1]/div[2]/div[2]/div[1]/div/h3/a')
@@ -28,13 +32,17 @@ button0.click()
 button = driver.find_element(By.XPATH, '/html/body/div[1]/header/div[3]/div[2]/a')
 button.click()
 logger.warning('Jakieś ostrzeżenie')
-
+# go to login site
 button1 = driver.find_element(By.XPATH, '//*[@id="menu_additional"]/a')
 
-button2 = driver.find_element(By.XPATH('//*[@id="user_login"]')).send_keys("login")
-button3 = driver.find_element(id('user_pass')).send_keys("login")
-logger.error('Jakiś Error')
-
+# button2 = driver.find_element(By.XPATH('//*[@id="user_login"]')).send_keys("login")
+# button3 = driver.find_element(id('user_pass')).send_keys("login")
+# logger.error('Jakiś Error')
+username = "cokolwiek"
+userpassword = "cefsds"
+driver.get("https://armyworld.pl/signin.php")
+driver.find_element(By.ID,'user_login').send_keys(username)
+driver.find_element(By.ID,'user_pass').send_keys(userpassword)
 
 
 driver.close()
